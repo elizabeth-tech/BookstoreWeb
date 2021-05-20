ccdfdf1530e46c01caf1440dd4b53b0eb3724a4b
elizabeth-tech <elizabeth-tech@gmail.com>
2021-05-20 18:58:17 +0700

    Создание репозитория книг

---
+++ BookstoreWeb/Models/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookstoreWeb.Models.Entities;
+using BookstoreWeb.Models.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookstoreWeb.Models
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookstoreDbContext context;
+
+        public IQueryable<Book> Books => context.Books;
+
+        public BookstoreRepository(BookstoreDbContext context) => this.context = context;
+
+        public void SaveBook(Book book) => context.SaveChanges();
+
+        public void CreateBook(Book book)
+        {
+            context.Add(book);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Book book)
+        {
+            context.Remove(book);
+            context.SaveChanges();
+        }
+    }
+}
--- BookstoreWeb/Startup.cs
+++ BookstoreWeb/Startup.cs
@@ -1,12 +1,10 @@
+using BookstoreWeb.Models;
+using BookstoreWeb.Models.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookstoreWeb
 {
@@ -15,6 +13,9 @@ namespace BookstoreWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Добавление служб репозиториев
+            services.AddScoped<IBookRepository, BookRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
