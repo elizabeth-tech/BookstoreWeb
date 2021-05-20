0cb1ed2e6d7bc444780052deb164c22fbf225011
elizabeth-tech <elizabeth-tech@gmail.com>
2021-05-20 19:04:36 +0700

    Создан основной контроллер, загружающий данные книг

---
+++ BookstoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using BookstoreWeb.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public HomeController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public ViewResult Index()
+        {
+            return View(bookRepository.Books);
+        }
+    }
+}
