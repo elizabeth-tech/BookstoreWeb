158052a7f3aab926f2309bea699e879cc7a703a2
elizabeth-tech <elizabeth-tech@gmail.com>
2021-05-21 23:56:43 +0700

    Реализован виджет корзины и возврат в корзину

---
+++ BookstoreWeb/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using BookstoreWeb.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreWeb.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService) => cart = cartService;
+
+        public IViewComponentResult Invoke() => View(cart);
+    }
+}
